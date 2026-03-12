29d77f86b7f8789972023070e225b4dca261be79
umapoggi <upoggisoqueff@udesa.edu.ar>
2026-03-12 15:42:07 -0300

    Update Funciones.py
    Hice funcion registrar_habitos

--- Funciones.py
+++ Funciones.py
@@ -5,3 +5,12 @@ Created on Thu Mar 12 15:17:01 2026
 @author: justo
 """
 
+def registrar_habitos():
+    lista_act = []
+    while True:
+        actividad = input("Ingrese actividad realizada ")
+        lista_act.append(actividad)
+        if actividad == "Termine":
+            break
+    
+     
