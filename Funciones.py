a838be97ef0cc113674b506102ee689204a453c0
vazpeitia <vazpeitia@udesa.edu.ar>
2026-03-17 16:04:48 -0300

    Update Funciones.py
    Hice docstring

--- Funciones.py
+++ Funciones.py
@@ -13,7 +13,21 @@ def registrar_habitos():
         if actividad == "Termine":
             break
     return lista_act
-    
+
+"""
+La funcion registrar habitos va a solicitarle al usuario que ingrese sus actividades, \
+con ellas va a realizar una lista donde especifique dichas actividades. La funcion se va a ver interrumpida\
+cuando el ususario especifique que termino.
+
+Parámetros
+----------
+No hay parametro, se llama a la funcion para obtener ejecutar el input
+
+Returns
+-------
+lista
+la devolucion de esta funcion va a ser la lista con las actividades que realiza usualmente
+"""    
     
 def analizar_habitos(lista): 
     diccionario_actividades = {}
@@ -25,6 +39,18 @@ def analizar_habitos(lista):
      
     return diccionario_actividades       
             
-           
-    
+"""
+La funcion analizar habitos va a recibir una lista de activiadades.\
+luego va a recorrer la lista y determinar la frecuencia de realizacion de estas actividades.
+
+Parámetros
+----------
+lista = lista
+Se recibe una lista resultante de otra funcion donde se determinan las actividades del usuario
+
+Returns
+-------
+diccionario
+la devolucion de esta funcion va a ser un diccionario con las actividades y la cantidad de veces que se realizo
+"""  
     
